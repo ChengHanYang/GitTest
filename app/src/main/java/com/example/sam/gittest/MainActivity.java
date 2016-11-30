8888ac3d775a9f0870810da560356998e9e51b27
ChengHanYang <ms0431581@gmail.com>
2016-11-30 20:38:33 +0800

    1111

--- app/src/main/java/com/example/sam/gittest/MainActivity.java
+++ app/src/main/java/com/example/sam/gittest/MainActivity.java
@@ -3,11 +3,10 @@ package com.example.sam.gittest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
+
 public class MainActivity extends AppCompatActivity {
-
-
-
-
 
 
 
